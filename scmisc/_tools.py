2161892a332c11d17eb7fa64fdadcfa005f7a6ad
Diego Diez <diego10ruiz@gmail.com>
2024-10-18 00:30:14 +0900

    Add subcluster_group_scvi and fix _pca version.

--- scmisc/_tools.py
+++ scmisc/_tools.py
@@ -1,13 +1,53 @@
-def subcluster_group(adata, var, group, layer, resolution=0.4):
+def subcluster_group_pca(adata, source_key, group, target_key, layer=None, n_pcs=10, resolution=0.4, seed=0):
   import scanpy as sc
-  cells = adata.obs[var] == group
-  tmp = adata[cells, :].copy()
-  sc.pp.pca(tmp)
-  sc.pp.neighbors(tmp, n_pcs=10)
-  sc.tl.leiden(tmp, key_added="_subcluster", resolution=resolution)
+  import numpy as np
 
-  adata.obs[var] = adata.obs[var].to_list()
-  adata.obs[var][cells] = adata.obs[var][cells] + "_" + tmp.obs["_subcluster"].to_list()
+  target_key_tmp = "." + target_key
+
+  cells = adata.obs_names[adata.obs[source_key] == group].to_list()
+  tmp = adata[cells, :].copy()
+
+  if layer is not None:
+    tmp.X = tmp.layers[layer]
+
+  sc.pp.pca(tmp)
+  
+  sc.pp.neighbors(tmp, n_pcs=n_pcs)
+  sc.tl.leiden(tmp, key_added=target_key_tmp, resolution=resolution, flavor="igraph", n_iterations=2, directed=False)
+  
+  if np.any(adata.obs.columns == target_key):
+     adata.obs[target_key] = adata.obs[target_key].to_list()
+  if ~np.any(adata.obs.columns == target_key):
+      adata.obs[target_key] = adata.obs[source_key].to_list()
+    
+  adata.obs.loc[cells, target_key] = adata.obs.loc[cells, target_key] + "_" + tmp.obs[target_key_tmp].to_list()
+
+def subcluster_group_scvi(adata, source_key, group, target_key, layer="counts", resolution=0.4, batch_key=None, categorical_covariate_keys=None, seed=0):
+  import scanpy as sc
+  import numpy as np
+  import scvi
+
+  scvi.settings.seed = seed
+
+  target_key_tmp = "." + target_key
+
+  cells = adata.obs_names[adata.obs[source_key] == group].to_list()
+  tmp = adata[cells, :].copy()
+
+  scvi.model.SCVI.setup_anndata(tmp, layer=layer, batch_key=batch_key, categorical_covariate_keys=categorical_covariate_keys)
+  model = scvi.model.SCVI(tmp)
+  model.train(max_epochs=400, early_stopping=True, early_stopping_monitor="validation_loss")
+  tmp.obsm["X_scvi"] = model.get_latent_representation()
+
+  sc.pp.neighbors(tmp, use_rep="X_scvi")
+  sc.tl.leiden(tmp, key_added=target_key_tmp, resolution=resolution, flavor="igraph", n_iterations=2, directed=False)
+
+  if np.any(adata.obs.columns == target_key):
+     adata.obs[target_key] = adata.obs[target_key].to_list()
+  if ~np.any(adata.obs.columns == target_key):
+      adata.obs[target_key] = adata.obs[source_key].to_list()
+    
+  adata.obs.loc[cells, target_key] = adata.obs.loc[cells, target_key] + "_" + tmp.obs[target_key_tmp].to_list()
 
 def calculate_dims(ngroups, nrows=None, ncols=None):
   import numpy as np
