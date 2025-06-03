f6cbe8a72b7788fb575243d8cd0f171bd76bee7b
Diego Diez <diego10ruiz@gmail.com>
2025-06-03 12:05:54 +0900

    Clean up.

--- scmisc/_data.py
+++ scmisc/_data.py
@@ -1,6 +1,7 @@
-from importlib.resources import files
 
 def load_10x_doublet_rate():
+  from importlib.resources import files
   import pandas as pd
+  
   path = files(__name__).joinpath("datasets/10x_doublets.csv")
   return pd.read_csv(path)
