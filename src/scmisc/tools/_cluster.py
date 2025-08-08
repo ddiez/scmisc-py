a43801428d92aa81d9c35b103683516bec3984f1
Diego Diez <diego10ruiz@gmail.com>
2025-08-08 10:45:10 +0900

    Move subcluster_group_pca from tools to cluster module in tools submodule.

--- src/scmisc/tools/_cluster.py
+++ src/scmisc/tools/_cluster.py
@@ -1,10 +1,63 @@
-def subcluster_group(adata, var, group, layer, resolution=0.4):
-  import scanpy as sc
-  cells = adata.obs[var] == group
-  tmp = adata[cells, :].copy()
-  sc.pp.pca(tmp)
-  sc.pp.neighbors(tmp, n_pcs=10)
-  sc.tl.leiden(tmp, key_added="_subcluster", resolution=resolution)
+def subcluster_group(adata, reduction_method, source_key, group, target_key, layer, **kargv):
+  import numpy as np
 
-  adata.obs[var] = adata.obs[var].to_list()
-  adata.obs[var][cells] = adata.obs[var][cells] + "_" + tmp.obs["_subcluster"].to_list()
+  if ~np.any(adata.obs.columns == target_key):
+     adata.obs[target_key] = adata.obs[source_key]
+
+
+  cells = adata.obs_names[adata.obs[source_key] == group].to_list()
+  tmp = adata[cells, :].copy()
+
+  if layer is not None:
+    tmp.X = tmp.layers[layer]
+  
+  match reduction_method:
+    case "pca":
+        subclusters = subcluster_group_pca(tmp, **kargv)
+    
+    case "harmony":
+        subclusters = subcluster_group_harmony(tmp, **kargv)
+    
+    case "scvi":
+        subclusters = subcluster_group_scvi(tmp, **kargv)
+  
+  if np.any(adata.obs.columns == target_key):
+     adata.obs[target_key] = subclusters
+  if ~np.any(adata.obs.columns == target_key):
+      adata.obs[target_key] =subclusters
+    
+  adata.obs.loc[cells, target_key] = adata.obs.loc[cells, target_key] + "_" + tmp.obs[target_key_tmp].to_list()
+  
+
+def subcluster_group_pca(adata, n_pcs=10, resolution=0.4):
+  import scanpy as sc
+  import numpy as np
+
+  sc.pp.pca(adata)
+  
+  sc.pp.neighbors(adata, n_pcs=n_pcs)
+  sc.tl.leiden(adata, key_added=".subcluster", resolution=resolution, flavor="igraph", n_iterations=2, directed=False)
+
+  return adata.obs[".subcluster"].to_list()
+
+def subcluster_group_harmony(adata, resolution=0.4):
+  # TODO: add support
+  import warnings
+  warnings.warn("Not supported yet")
+
+def subcluster_group_scvi(adata, source_key, group, target_key, layer="counts", resolution=0.4, batch_key=None, categorical_covariate_keys=None, seed=0):
+  import scanpy as sc
+  import numpy as np
+  import scvi
+
+  scvi.settings.seed = seed
+
+  scvi.model.SCVI.setup_anndata(adata, layer=layer, batch_key=batch_key, categorical_covariate_keys=categorical_covariate_keys)
+  model = scvi.model.SCVI(adata)
+  model.train(max_epochs=400, early_stopping=True, early_stopping_monitor="validation_loss")
+  adata.obsm["X_scvi"] = model.get_latent_representation()
+
+  sc.pp.neighbors(adata, use_rep="X_scvi")
+  sc.tl.leiden(adata, key_added=".subcluster", resolution=resolution, flavor="igraph", n_iterations=2, directed=False)
+
+  return adata.obs[".subcluster"].to_list()
